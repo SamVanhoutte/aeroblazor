165f016e801d38cb64dc1301beef504a54764c81
Sam Vanhoutte <sam.vanhoutte@marchitec.be>
2026-01-24 07:27:34 +0100

    Add file size printing

--- src/AeroBlazor/Extensions/DisplayExtensions.cs
+++ src/AeroBlazor/Extensions/DisplayExtensions.cs
@@ -124,6 +124,19 @@ public static class DisplayExtensions
         return numberValue;
     }
     
+    public static string PrintFileSize(this long bytes)
+    {
+        string[] sizes = { "B", "KB", "MB", "GB" };
+        double len = bytes;
+        int order = 0;
+        while (len >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            len = len / 1024;
+        }
+        return $"{len:0.##} {sizes[order]}";
+    }
+    
     
     public static string PrintDecimal(this decimal? value, string? suffix = null)
     {
